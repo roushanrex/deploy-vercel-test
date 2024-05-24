42e34be4e8f7bfcb7b67c9a510bbf349628f73d2
Roushan <aroushan9@gmail.com>
2024-05-24 21:18:24 +0530

    init

--- myweb/settings.py
+++ myweb/settings.py
@@ -73,16 +73,25 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'GzCkMixulXwWnIPYlkex',
+#         'HOST': 'containers-us-west-92.railway.app',
+#         'PORT': '5526',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'GzCkMixulXwWnIPYlkex',
-        'HOST': 'containers-us-west-92.railway.app',
-        'PORT': '5526',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 
